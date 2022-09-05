e8ebd4d5c95b1d6984a2340fc2b478741734d2e8
UkemeSkywalker <skywalkershadow9@gmail.com>
2022-09-05 04:06:13 +0100

    folder setup

---
+++ src/hardhat.config.ts
@@ -0,0 +1,18 @@
+require("dotenv").config();
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: process.env.RPC,
+      accounts: [process.env.SKYWALKER]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN
+  }
+};
+
+
